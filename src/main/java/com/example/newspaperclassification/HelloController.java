f872829863b9dd5f5cf07dbcda741a8ca7d0166b
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2023-12-04 10:19:28 +0600

    UI complete without connected

--- src/main/java/com/example/newspaperclassification/HelloController.java
+++ src/main/java/com/example/newspaperclassification/HelloController.java
@@ -2,13 +2,33 @@ package com.example.newspaperclassification;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label welcomeText;
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Label classificationLabel;
+
+    @FXML
+    protected void onClassifyButtonClick() {
+       String inputText =  textArea.getText();
+        System.out.println("Input text is: "+inputText);
+
+        classificationLabel.setText("Level found");
+    }
+
+    @FXML
+    protected void textFieldController(){
+        classificationLabel.setText("");
+    }
+
+    @FXML
+    protected void addTrainingSectionCheckBox(){
+        System.out.println("Check box clicked");
     }
 }
